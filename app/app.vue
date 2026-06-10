57ab14d4061a32e73b87987b414d2fcb91c1cf65
Joran Quinten <joran@joranquinten.nl>
2026-06-10 09:52:24 -0400

    feat: adds background image

---
+++ app/app.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <NuxtPage />
+    <div class="background-container">
+      <NuxtImg
+        src="/images/background.webp"
+        class="background"
+        sizes="100vw sm:100vw md:100vw lg:100vw xl:100vw"
+        densities="x1"
+        quality="35"
+        loading="eager"
+        fetchpriority="low"
+        alt=""
+        aria-hidden="true"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.background-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+
+.background {
+  filter: blur(5px);
+  position: absolute;
+  inset: -10px;
+  width: calc(100% + 20px);
+  height: calc(100% + 20px);
+  object-fit: cover;
+  z-index: -1;
+}
+</style>
